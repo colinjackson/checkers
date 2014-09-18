4bfe315d52aa69dd2d735c5700aca54915a3254b
Colin <colin.s.jackson90@gmail.com>
2014-09-18 12:46:35 -0400

    Add piece sliding and jumping logic

--- board.rb
+++ board.rb
@@ -1,40 +1,45 @@
 load './piece.rb'
 
 class Board
-  attr_reader :rows
+  attr_reader :rows, :board_size
 
   def self.make_rows(board_size)
     Array.new(board_size) { Array.new(board_size) }
   end
 
   def initialize(set_pieces = true, board_size = 8)
+    @board_size = board_size
     @rows = Board.make_rows(board_size)
     add_pieces if set_pieces
   end
 
-  def [](pos)
-    row, col = pos
+  def [](square)
+    row, col = square
     rows[row][col]
   end
 
-  def []=(pos, value)
-    row, col = pos
+  def []=(square, value)
+    row, col = square
     rows[row][col] = value
   end
 
-  def add_piece(piece, pos)
-    self[pos] = piece
-    piece.pos = pos
+  def add_piece(piece, square)
+    self[square] = piece
+    piece.square = square
   end
 
   def add_pieces
-    ranges = get_piece_ranges
-    white_range = ranges[0]
+    white_range, black_range = get_piece_ranges
 
     self.rows.each_with_index do |row, row_i|
-      next unless ranges.any? { |range| range.include?(row_i) }
+      if white_range.include?(row_i)
+        color = :white
+      elsif black_range.include?(row_i)
+        color = :black
+      else
+        next
+      end
 
-      color = white_range.include?(row_i) ? :white : :black
       set_row(row, row_i, color)
     end
   end
@@ -50,8 +55,8 @@ class Board
     row.count.times do |col_i|
       next unless (row_i + col_i).odd?
 
-      pos = [row_i, col_i]
-      Piece.new(self, color, pos)
+      square = [row_i, col_i]
+      Piece.new(self, color, square)
     end
   end
 end
--- piece.rb
+++ piece.rb
@@ -1,19 +1,90 @@
 class Piece
-  attr_reader :board, :color
-  attr_writer :pos
+  UP_SLIDES, DOWN_SLIDES = [[-1, -1], [-1, 1]], [[1, -1], [1, 1]]
 
-  def initialize(board, color, pos)
+  attr_reader :board, :color
+  attr_writer :square
+
+  def initialize(board, color, square)
     @board, @color = board, color
-    board.add_piece(self, pos)
+    board.add_piece(self, square)
 
     @king = false
+  end
+
+  def perform_slide(to_square)
+    return false unless valid_move?(to_square)
+
+    update_position(to_square)
+    true
+  end
+
+  def perform_jump(to_square)
+    return false unless valid_move?(to_square)
+
+    jumped = jumped_square(to_square)
+    board[jumped] = nil
+    update_position(to_square)
+    true
+  end
+
+  def update_position(to_square)
+    board[self.square] = nil
+    board.add_piece(self, to_square)
+  end
+
+  def slides
+    return UP_SLIDES + DOWN_SLIDES if king?
+    color == :black ? UP_SLIDES : DOWN_SLIDES
+  end
+
+  def jumps
+    slides.map do |slide|
+      slide.map { |dir| dir * 2 }
+    end
+  end
+
+  def valid_move?(to_square)
+    return false unless board[to_square].nil?
+
+    if reachable_squares(slides).include?(to_square)
+      true
+    elsif reachable_squares(jumps).include?(to_square)
+      jumped = jumped_square(to_square)
+      board[jumped] && board[jumped].color != self.color
+    else
+      false
+    end
+  end
+
+  def reachable_squares(moves)
+    reachable_squares = []
+
+    row_i, col_i = self.square
+    moves.each do |move|
+      d_row, d_col = move
+      square = [row_i + d_row, col_i + d_col]
+      next unless square.all? { |i| i.between?(0, board.board_size - 1) }
+
+      reachable_squares << square
+    end
+
+    reachable_squares
+  end
+
+  def jumped_square(to_square)
+    from_row, from_col = self.square
+    to_row, to_col = to_square
+
+    mid_row = (from_row + to_row) / 2
+    mid_col = (from_col + to_col) / 2
+    [mid_row, mid_col]
   end
 
   def king?
     @king
   end
 
-  def king_me
+  def king_me!
     @king = true
   end
 
